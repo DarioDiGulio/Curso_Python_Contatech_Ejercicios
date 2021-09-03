7bb16d4916a6bbea102ef2d4ef491f80a99ee515
Dario Di Gulio <dariodigulio@gmail.com>
2021-09-02 23:16:56 -0300

    done

--- src/infrastructure/SystemIO.py
+++ src/infrastructure/SystemIO.py
@@ -3,7 +3,7 @@ from src.infrastructure.IO import IO
 
 class SystemIO(IO):
     def input(self, text, data_type=str):
-        entry = input(f'{text}\n')
+        entry = input(f"{text}\n")
         return IO.parse_text(entry, data_type)
 
     def output(self, text):
